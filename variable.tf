326cfb88094e2391bbda99c57827bb2394f006a8
rahi8 <54687347+rahi8@users.noreply.github.com>
2021-03-12 20:49:22 -0800

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "subscription_id" {
+  default     = "a6d28404-8582-4c31-946b-05b92d73d337"
+}
+variable "tenant_id" {
+  default     = "08c4f2ee-ae3c-4650-8a5f-548eae86282b"
+}
+variable "client_id" {
+  default     = "7163176f-1efd-4e43-9681-01a50e12a000"
+}
+variable "location" {
+  default     = "westus"
+}
+variable "resourceGroupName" {
+  default     = "new"
+}
+variable "client_secret" {
+  default     = "D7co~i-l7CVzoP2.w4QEdKOrHFd2u-d-.7"
+}
